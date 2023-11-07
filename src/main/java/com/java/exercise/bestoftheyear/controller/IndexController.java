f52afe77755676b1c389ace58ec3072da606284a
MirkoGLostia <giggi.torre@mail.com>
2023-11-07 14:53:53 +0100

    home page with controller

--- src/main/java/com/java/exercise/bestoftheyear/controller/IndexController.java
+++ src/main/java/com/java/exercise/bestoftheyear/controller/IndexController.java
@@ -1,4 +1,18 @@
 package com.java.exercise.bestoftheyear.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class IndexController {
+
+    @GetMapping("home")
+    public String home(Model model) {
+        String name = "Mirko";
+        model.addAttribute("ownerName", name);
+        return "home-page";
+    }
 }
